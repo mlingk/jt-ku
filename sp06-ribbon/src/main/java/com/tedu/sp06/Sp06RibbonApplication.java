e5a746d4c45d659d645501aca88b83a869e699d9
Tarena <Tarena@18-4-46-176.dyn.estpak.ee>
2019-06-28 11:15:16 +0800

    ribbon

---
+++ sp06-ribbon/src/main/java/com/tedu/sp06/Sp06RibbonApplication.java
@@ -0,0 +1,31 @@
+package com.tedu.sp06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Sp06RibbonApplication {
+
+		//生成一个动态代理对象,切入负载均衡代码.
+		@LoadBalanced
+		//创建 RestTemplate 实例，并存入 spring 容器
+		@Bean
+		public RestTemplate getRestTemplate() {
+			//设置 RestTemplate 的请求工厂的超时属性
+			SimpleClientHttpRequestFactory f = new SimpleClientHttpRequestFactory();
+			f.setConnectTimeout(1000);
+			f.setReadTimeout(1000);//接收响应数据的超时时间
+			return new RestTemplate(f);
+			//return new RestTemplate();
+		}
+		
+	public static void main(String[] args) {
+		SpringApplication.run(Sp06RibbonApplication.class, args);
+	}
+
+}
